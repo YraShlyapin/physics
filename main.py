79a64755b7b6026de5849233c52c7dab9eb63ec9
Yura <shlyapin.y.a@gmail.com>
2022-05-28 16:59:37 +0400

    первый коммит

---
+++ main.py
@@ -0,0 +1,24 @@
+from Physics import Physics
+from number import number
+
+def factorial(i):
+    sum=1
+    for _ in range(1,i):
+        sum*=(_+1)
+    return sum
+
+def procent(first,second):
+    return first/second*100
+
+def mark(all:int,true_answer:int):
+    a = procent(true_answer,all)
+    if a>=85:
+        return 5
+    if a>=65:
+        return 4
+    if a>=45:
+        return 3
+    return 2
+
+
+print(Physics.alpha_decay(92,238))
